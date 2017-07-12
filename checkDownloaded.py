d9018ec3898476fb217b09ccc0059411bd91ae8d
bg <bg@example.com>
2017-07-12 17:21:00 +0800

    cookie

--- checkDownloaded.py
+++ checkDownloaded.py
@@ -1,6 +1,6 @@
 def checkDownloaded():
     downloaded = set()
-    file_object = open('mj.txt', 'r')
+    file_object = open(r'D:\pythonResouce\mj.txt', 'r')
     for line in file_object:
         downloaded.add(line[:-1])
     #print(downloaded)
--- mj.py
+++ mj.py
@@ -5,19 +5,19 @@ import checkDownloaded
 import account
 import WorkInTime
 
-header = {
-    'Connection': 'Keep-Alive',
-    'Accept': 'application/json, text/javascript, */*',
-    'Accept-Language': 'zh-CN;q=0.01',
-    'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; Tablet PC 2.0)',
-    'Accept-Encoding': 'gzip, deflate',
-    'Host': 'www.zmz2017.com',
-    'DNT': '1',
-    'Cookie': 'mykeywords=a%3A1%3A%7Bi%3A0%3Bs%3A15%3A%22%E9%80%9F%E5%BA%A6%E4%B8%8E%E6%BF%80%E6%83%85%22%3B%7D; PHPSESSID=andogol10vupq02c6492g9d0l1; GINFO=uid%3D3560304%26nickname%3Dlojl%26group_id%3D1%26avatar_t%3Dhttp%3A%2F%2Ftu.zmzjstu.com%2Fftp%2Favatar%2Ff_noavatar_t.gif%26main_group_id%3D0%26common_group_id%3D59; GKEY=907d0ae24780b1b2d163943e544c4736'
-}
 
 def loginAndDownload():  # 登陆函数
 
+    header = {
+        'Connection': 'Keep-Alive',
+        'Accept': 'application/json, text/javascript, */*',
+        'Accept-Language': 'zh-CN;q=0.01',
+        'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; Tablet PC 2.0)',
+        'Accept-Encoding': 'gzip, deflate',
+        'Host': 'www.zmz2017.com',
+        'DNT': '1',
+        'Cookie': 'PHPSESSID=10uqu72lv57jhl9l58okiei1n1; GINFO=uid%3D3560304%26nickname%3Dlojl%26group_id%3D1%26avatar_t%3Dhttp%3A%2F%2Ftu.zmzjstu.com%2Fftp%2Favatar%2Ff_noavatar_t.gif%26main_group_id%3D0%26common_group_id%3D59; GKEY=ef12f18badab1ad2a170d894c20aee44'
+    }
     url = 'http://www.zmz2017.com/User/Login/ajaxLogin'
 
     login_seesion = requests.Session()
@@ -36,8 +36,8 @@ def loginAndDownload():  # 登陆函数
     #print(soup.prettify())
 
     # 第一次运行添加这两个文件
-    file_object = open('mj.txt', 'a')
-    fileNewDownload = open('newmj.txt', 'a')
+    file_object = open(r'D:\pythonResouce\mj.txt', 'a')
+    fileNewDownload = open(r'D:\pythonResouce\newmj.txt', 'a')
     for link in soup.find_all(href=re.compile("ed2k")):
         #print(type(link))
         #print(link.attrs)
